f980b22643e61e796ee836197b34c4fb622e5805
Zane Smith <zane@webfarms.co.za>
2018-08-28 09:27:03 +0200

    initial commit
    Manager App Creation
    Messenger App Creation
    Web Socket Creation
    Test App Creation (hello world)

---
+++ hello world/Test/platforms/browser/www/testingPHP.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+<?php
+/**
+ * Created by PhpStorm.
+ * User: WebFarms
+ * Date: 8/27/2018
+ * Time: 7:48 PM
+ */
+
+$test = "bengy";
+
+echo $test;
+
+?>
+</body>
+</html>
