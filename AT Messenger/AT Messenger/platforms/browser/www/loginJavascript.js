aec82b7ce1603185cfc50a69db714e54bae45b9f
sajjaadishmail3 <sajjaadishmail@gmail.com>
2018-10-26 00:19:48 +0200

    Just added few things.

--- AT Messenger/AT Messenger/platforms/browser/www/loginJavascript.js
+++ AT Messenger/AT Messenger/platforms/browser/www/loginJavascript.js
@@ -1,45 +1,82 @@
 //making a connection
-var socket = io.connect('192.168.0.63:4000');
+// var socket = io.connect('192.168.0.63:4000');
+
+//testing login
+var socket  = io.connect('10.0.0.3:3002');
+//
+// var mysql = require('mysql');
+//
+// var con = mysql.createConnection({
+//     host: "localhost",
+//     user: "root",
+//     password: "",
+//     database: "at_messengerdb"
+// });
+//
+// console.log('Running');
+//
+// // con.connect(function(err) {
+// //     if (err) throw err;
+// //     console.log("Connected!");
+// // });
+//
+// // document.getElementById('login').addEventListener("click", function () {
+// //     alert("Working");
+// //     // con.connect(function(err) {
+// //     //     if (err) throw err;
+// //     //     con.query("SELECT * FROM client", function (err, result, fields) {
+// //     //         if (err) throw err;
+// //     //         console.log(result);
+// //     //     });
+// //     // });
+// // })
+//
+// function btnClick()
+// {
+//     con.connect(function(err) {
+//         if (err) throw err;
+//         console.log("Connected!");
+//     });
+//     // alert("Button working!!!");
+//     // con.connect(function(err) {
+//     //     if (err) throw err;
+//     //     con.query("SELECT * FROM client", function (err, result, fields) {
+//     //         if (err) throw err;
+//     //         console.log(result);
+//     //     });
+//     // });
+// }
 
 // getting dom
 var username = document.getElementById('username'),
     password = document.getElementById('password'),
-    login = document.getElementById('login'),
-    name = document.getElementById('name'),
-    surname = document.getElementById('surname'),
-    cellNumber = document.getElementById('cellNumber'),
-    email = document.getElementById('email'),
-    password1 = document.getElementById('password1'),
-    password2 = document.getElementById('password2'),
-    register = document.getElementById('register');
+
+    login = document.getElementById('login');
 
 //emit event
     //login page
-        login.addEventListener('click',function(){
-            socket.emit('login',{
-                username: username.value,
-                password: password.value,
-            });
+    login.addEventListener('click',function(){
+        socket.emit('login',{
+            username: username.value,
+            password: password.value,
         });
-    //register page
-        register.addEventListener('click',function(){
-            if (password1 == password2){
-                socket.emit('register',{
-                    name : name.value,
-                    surname : surname.value,
-                    cellNumber : cellNumber.value,
-                    email : email.value,
-                    password1 : password1.value,
-                });
-            }else {
-                alert("passwords do not match");
-            }
+    });
 
-        });
+    //register page
+//original
+// message.addEventListener('keypress',function(){
+//     socket.emit('typing', handle.value);
+// });
+// btn.addEventListener('click',function(){
+//     socket.emit('chat',{
+//         message: message.value,
+//         handle: handle.value,
+//     });
+// });
 
 //listen for events
     //login
-        socket.on('response',function (data) {
+    socket.on('response',function (data) {
             if(data.status == 'accepted'){
                 window.location = "layout.html";
             }else{
@@ -47,21 +84,10 @@ var username = document.getElementById('username'),
             }
 
         });
-    //Register
-        socket.on('insertResponse',function (data) {
-            if(data.status == 'inserted'){
-                window.location = "index.html";
-            }else if (data.status == 'user exists'){
-                alert("user exists");
-            }
-
-        });
-
-
-// socket.on('chat',function(data){
-//     feedback.innerHTML = "";
-//     output.innerHTML += '<p><strong>' + data.handle + ': <br/></strong>' + data.message + '</p>';
-// });
-// socket.on('typing',function(data){
-//     feedback.innerHTML = '<p><em>'+data+' is typing a message</em></p>';
-// });
+socket.on('chat',function(data){
+    feedback.innerHTML = "";
+    output.innerHTML += '<p><strong>' + data.handle + ': <br/></strong>' + data.message + '</p>';
+});
+socket.on('typing',function(data){
+    feedback.innerHTML = '<p><em>'+data+' is typing a message</em></p>';
+});
