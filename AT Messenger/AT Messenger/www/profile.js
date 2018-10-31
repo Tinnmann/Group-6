d15328802f1b6ec005009bc520f91cb726e07e10
Dayne Deedat <dayne.deedat@gmail.com>
2018-10-31 19:03:53 +0200

    Profile Page Updated
    - Added a feedback message when the "save" button is  pushed

--- AT Messenger/AT Messenger/www/profile.js
+++ AT Messenger/AT Messenger/www/profile.js
@@ -16,6 +16,7 @@ logout.addEventListener('click',function(){
    window.location = 'index.html';
 });
 save.addEventListener('click',function(){
+    
     socket.emit('profileUpdate',{
         id : 1,
         email : email.value,
@@ -23,10 +24,24 @@ save.addEventListener('click',function(){
         profileSurname : profileSurname.value,
         profileName : profileName.value,
     });
+    
+    //Inserts success feedback message
+    document.getElementById("savemessage").innerHTML = "
+        <div class="alert alert-success alert-dismissible fade show">
+            <button type="button" class="close" data-dismiss="alert">&times;</button>
+            <strong>Changes Saved</strong>
+        </div>";
 });
+
 socket.on('profilePopulate',function (data) {
     profileName.value = data.result[0]['client_name'];
     profileSurname.value = data.result[0]['surname'];
     profileCell.value = data.result[0]['address'];
     email.value = data.result[0]['email'];
 });
+
+
+<div class="alert alert-success alert-dismissible">
+  <button type="button" class="close" data-dismiss="alert">&times;</button>
+  <strong>Success!</strong> Indicates a successful or positive action.
+</div>
