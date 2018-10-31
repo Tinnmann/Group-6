c1e72a4866c7bd4c14cdf7d6d6173ed35c699cff
Dayne Deedat <dayne.deedat@gmail.com>
2018-10-31 23:12:40 +0200

    LogTicket
    validation

--- AT Messenger/AT Messenger/www/logTicket.js
+++ AT Messenger/AT Messenger/www/logTicket.js
@@ -1,5 +1,10 @@
 var socket = io.connect('192.168.0.63:4000');
 var logTicketButton = document.getElementById('logTicket');
+var cat = document.getElementById('catSelect');
+var selected = cat.options[cat.selectedIndex].value;
+var msg = document.getElementById('message');
+var errorMessage = "";
+
 window.onload = selectInserter();
 function selectInserter(){
     socket.emit('populate',{
@@ -19,17 +24,45 @@ socket.on('selectPopulate',function (data) {
 });
 
 logTicketButton.addEventListener('click', function(){
-    var cat = document.getElementById('catSelect'),
-        selected = cat.options[cat.selectedIndex].value,
-        message = document.getElementById('message');
-    if(message.value != '' && selected != 'Select Category' ){socket.emit('log',{
-        cat : selected,
-        message : message.value,
-        id : 1,
-        name : 'john',
-    });
+    errorMessage = "";
+    
+    var catError = validateCat();
+    var msgError = validateMsg();
+    
+    if(catError && msgError){
+        socket.emit('log',{
+            cat : selected,
+            message : msg.value,
+            id : 1,
+            name : 'john',
+        });
     }else{
-
+        document.getElementById("error").innerHTML='<div class="alert alert-danger">'+
+            errorMessage
+        '</div>';
     }
 
 });
+
+function validateCat(){
+    if(cat.value==""){
+        errorMessage += "Please select category<br>";
+        document.getElementById("catSelect").style.backgroundColor = "#ffaaaa";
+        return false;
+    }
+    else{
+        document.getElementById("catSelect").style.backgroundColor = "";
+        return true;
+    }
+}
+
+function validateMsg(){
+    if (msg.value === "            ") {
+        errorMessage += "Please enter message <br>";
+        document.getElementById("message").style.backgroundColor = "#ffaaaa";
+        return false;
+    } else {
+        document.getElementById("message").style.backgroundColor = "";
+        return true;
+    }
+}
